4fb6370bc1b7981d6531ae66a12500db5c97fc9f
Niranjan-Pawar-07 <158538221+Niranjan-Pawar-07@users.noreply.github.com>
2025-08-19 23:40:00 +0530

    fix  to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC44VIXNQdlLI3kfg_U0mvxKf32nf5AlpM",
-  authDomain: "interdepartmental-coordi-c8b9b.firebaseapp.com",
-  projectId: "interdepartmental-coordi-c8b9b",
-  storageBucket: "interdepartmental-coordi-c8b9b.firebasestorage.app",
-  messagingSenderId: "713124321439",
-  appId: "1:713124321439:web:59332439f626d8dbee2aaa",
-  measurementId: "G-R73JC6MVX2"
+  // your firebase config
 };
 
 const app = initializeApp(firebaseConfig);
